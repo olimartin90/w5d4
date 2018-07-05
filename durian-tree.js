b2965b459232485d212fb088edc06fad4432544d
Olivier Martin <olimartin90@gmail.com>
2018-07-05 23:34:16 +0000

    recursion and data structure - tree

---
+++ durian-tree.js
@@ -0,0 +1,64 @@
+class Employee {
+    constructor(name, title, salary) {
+        this.name = name;
+        this.title = title;
+        this.salary = salary;
+        this.boss = null,
+        this.subordinates = [];
+    }
+
+    addSubordinate(subordinate) {
+        this.subordinates.push(subordinate);
+        subordinate.boss = this;
+    }
+
+    get numberOfSubordinates() {
+        return this.subordinates.length;
+    }
+
+    get numberOfPeopleToCEO() {
+        let numberOfPeople = 0;
+        let currentEmployee = this;
+
+        while (currentEmployee.boss) {
+            currentEmployee = currentEmployee.boss;
+            numberOfPeople++;
+        }
+        return numberOfPeople;
+    }
+
+    hasSameBoss(employee) {
+        return this.boss === employee.boss;
+    }
+}
+
+const ada      = new Employee("Ada", "CEO", 3000000.00);
+const craig    = new Employee("Craig", "VP Software", 1000000);
+const arvinder = new Employee("Arvinder", "Chief Design Officer", 1000000);
+const angela   = new Employee("Angela", "VP Retail", 1000000);
+const phil     = new Employee("Phil", "VP Marketing", 1000000);
+
+const simone   = new Employee("Simone", "Employee", 60000);
+const ali      = new Employee("Ali", "Employee", 60000);
+const florida  = new Employee("Florida", "Employee", 60000);
+const david    = new Employee("David", "Employee", 60000);
+const brian    = new Employee("Brian", "Employee", 60000);
+const karla    = new Employee("Karla", "Employee", 60000);
+
+ada.addSubordinate(craig);
+ada.addSubordinate(arvinder);
+ada.addSubordinate(angela);
+ada.addSubordinate(phil);
+
+craig.addSubordinate(simone);
+craig.addSubordinate(ali);
+
+phil.addSubordinate(florida);
+phil.addSubordinate(david);
+phil.addSubordinate(brian);
+
+angela.addSubordinate(karla);
+
+console.log(craig.boss);
+console.log(craig.numberOfSubordinates);
+console.log(craig.numberOfPeopleToCEO);
